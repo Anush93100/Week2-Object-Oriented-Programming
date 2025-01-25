8835156835ac421f4472200d053b21a5a6e1cea8
Anush Tenguriya <anushtenguriya93100@gmail.com>
2025-01-25 13:40:47 +0530

    demonstrating encapsulation and polymorphism using E-Commerce platform

---
+++ Day6-Java-Encapsulation-Polymorphism/src/ecommerceplatform/ECommercePlatform2.java
@@ -0,0 +1,14 @@
+package ecommerceplatform;
+
+public class ECommercePlatform2 {
+    public static void main(String[] args) {
+        Electronics product1=new Electronics(101,"Ear phone",200,2,5);
+        product1.displayProductDetails();
+
+        Clothing product2=new Clothing(102,"Pant",1000,3,10);
+        product2.displayProductDetails();
+
+        Groceries product3=new Groceries(103,"Rice",120,2,4);
+        product3.displayProductDetails();
+    }
+}
